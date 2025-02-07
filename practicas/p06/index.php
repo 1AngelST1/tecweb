340998120073dce4f483fddb53deac9f75308c4b
1AngelST1 <angel.sarmiento0312@gmail.com>
2025-02-06 18:31:44 -0600

    se creo una carpeta (p06) dentro de esta se localiza un documeto php y otra carpeta src para guardar las funciones del documento php de la carpeta p06

---
+++ practicas/p06/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica 6</title>
+</head>
+<body>
+    <h2>Ejercicio 1</h2>
+    <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
+    <?php
+
+        require_once __DIR__ .'/src/funciones.php';
+        
+        if(isset($_GET['numero']))
+        {
+            es_multiplo7y5($_GET['numero']);
+        }
+
+    ?>
+
+
+</body>
+</html>
