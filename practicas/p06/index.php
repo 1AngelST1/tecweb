b9acfc0d4dcc2aac16fbc756c2b283acca70225a
1AngelST1 <angel.sarmiento0312@gmail.com>
2025-02-06 19:04:19 -0600

    se actualizaron los archivos php de la carpeta p06 para resolver los problemas 2 y 3

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -19,6 +19,52 @@
 
     ?>
 
+    <h2>Ejercicio 2</h2>
+    <p>Crea un programa para la generación repetitiva de 3 números aleatorios hasta obtener una
+    secuencia compuesta por:</p>
+    <p>
+        impar, par , impar <br>
+        990,   382,  786 <br>
+        422,   361,  473 <br>
+        213,   744,  911
+   </p>
+
+   <?php
+        require_once __DIR__ .'/src/funciones.php';
+        
+        list($matriz, $iteraciones) = generarSecuencias();
+
+        echo "<h3>Secuencias generadas:</h3>";
+        echo "<table border='1'>";
+        foreach ($matriz as $fila) {
+            echo "<tr><td>{$fila[0]}</td><td>{$fila[1]}</td><td>{$fila[2]}</td></tr>";
+        }
+        echo "</table>";
+
+        echo "<p>Total de iteraciones: $iteraciones</p>";
+    ?>
+
+
+    <h2>Ejercicio 3</h2>
+    <p>Utiliza un ciclo while para encontrar el primer número entero obtenido aleatoriamente,
+    pero que además sea múltiplo de un número dado.</p>
+
+    <?php
+
+        require_once __DIR__ .'/src/funciones.php';
+        
+        if(isset($_GET['multiplo']))
+        {
+            $multiplo = $_GET['multiplo'];
+            $numero = encontrarMultiplo($multiplo);
+            echo "<h3>El primer número múltiplo de $multiplo es: $numero</h3>";
+        }
+
+    ?>
+
+    
+
+
 
 </body>
 </html>
--- practicas/p06/src/funciones.php
+++ practicas/p06/src/funciones.php
@@ -1,5 +1,6 @@
 <?php
 
+//problema 1
 function es_multiplo7y5($num)
 {
     if ($num%5==0 && $num%7==0)
@@ -11,4 +12,34 @@ function es_multiplo7y5($num)
         echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
     }
 }
+
+//problema 2
+function generarSecuencias()
+{
+    $matriz = [];
+    $iteraciones = 0;
+    
+    while (true) {
+        $num1 = rand(1, 999);
+        $num2 = rand(1, 999);
+        $num3 = rand(1, 999);
+        
+        if ($num1 % 2 != 0 && $num2 % 2 == 0 && $num3 % 2 != 0) {
+            $matriz[] = [$num1, $num2, $num3];
+            if (count($matriz) >= 4) break;
+        }
+        $iteraciones++;
+    }
+    return [$matriz, $iteraciones];
+}
+
+//problema 3
+function encontrarMultiplo($n) {
+    do {
+        $random = rand(1, 1000);
+    } while ($random % $n != 0);
+    return $random;
+}
+
+
 ?>
