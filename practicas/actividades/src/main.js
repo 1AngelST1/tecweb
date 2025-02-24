785b1a7973b14d00f35dab8fdfd25bc74d2c32d9
1AngelST1 <angel.sarmiento0312@gmail.com>
2025-02-24 00:12:20 -0600

    se actualizo el archivo mian.js y el index para egregar lo que son div para que se muestren los resultados en la pagina y no en alertas

--- practicas/actividades/src/main.js
+++ practicas/actividades/src/main.js
@@ -14,7 +14,8 @@ function getDatos()
 
 function holamundo()
 {
-    alert("Hola Mundo");
+    var div = document.getElementById('holamundo');
+    div.innerHTML = '<h1> Hola Mundo </h1>';
 }
 
 function variable()
@@ -23,14 +24,28 @@ function variable()
     var edad = 20;
     var altura = 1.70;
     var casado = false;
-    alert("Nombre: "+nombre+" Edad: " +edad  +" Altura: "+altura+" Casado: "+casado);
+
+    var div = document.getElementById('nom');
+    div.innerHTML = '<h3> Nombre: '+nombre+'</h3>';
+
+    var div1 = document.getElementById('ed');
+    div1.innerHTML += '<h3> Edad: '+edad+'</h3>';
+
+    var div2 = document.getElementById('altura');
+    div2.innerHTML += '<h3> Altura: '+altura+'</h3>';
+
+    var div3 = document.getElementById('casado');
+    div3.innerHTML += '<h3> Casado: '+casado+'</h3>';
 }
 
 function variable2()
 {
-    var nombre = prompt("ingresa tu nombre: ", "");
-    var edad = prompt("ingresa tu edad: ", "");
-    alert("Hola: "+nombre+" , asi que tienes: " +edad+" años.");
+    var nom2 = prompt("ingresa tu nombre: ", "");
+    var edad2 = prompt("ingresa tu edad: ", "");
+
+    var div = document.getElementById('mensaje');
+    div.innerHTML = '<h3> hola '+nom2+' asi que tienes :' +edad2+ 'años </h3>';
+
 }
 
 function estructurada()
@@ -40,8 +55,12 @@ function estructurada()
     var suma = parseInt(numero) + parseInt(num);
     var multi = parseInt(numero) * parseInt(num);
 
-    alert("La suma de los numeros es: "+suma);
-    alert("La multiplicacion de los numeros es: "+multi);
+    var div = document.getElementById('suma');
+    div.innerHTML = '<h3> La suma de los numeros es: '+suma+'</h3>';
+
+    var div1 = document.getElementById('multi');
+    div1.innerHTML = '<h3> La multiplicacion de los numeros es: '+multi+'</h3>';
+
 }
 
 function sentenciaif()
@@ -51,7 +70,8 @@ function sentenciaif()
 
     if(nota >= 4)
     {
-        alert("Felicidades "+nombre+", has aprobado con: "+nota);
+        var div = document.getElementById('aprobado');
+        div.innerHTML = '<h3> Felicidades '+nombre+', has aprobado con: '+nota+'</h3>';
     }
 }
 
@@ -65,11 +85,13 @@ function sentenciaifelse()
 
     if(numero1 > numero2)
     {
-        alert("El numero mayor es: "+numero1);
+        var div = document.getElementById('mayor');
+        div.innerHTML = '<h3> El numero mayor es: '+numero1+'</h3>';
     }
     else
     {
-        alert("El numero mayor es: "+numero2);
+        var div1 = document.getElementById('mayor');
+        div1.innerHTML = '<h3> El numero mayor es: '+numero2+'</h3>';
     }
 }
 
@@ -87,17 +109,20 @@ function sentenciaifelseif()
 
     if (prome >= 7)
     {
-        alert("Aprobado con: "+prome);
+        var div = document.getElementById('califi');
+        div.innerHTML = '<h3> Aprobado con: '+prome+'</h3>';
     }
     else 
     {
         if (prome >= 4)
         {
-            alert("Regular con: "+prome);
+            var div1 = document.getElementById('califi');
+            div1.innerHTML = '<h3> Regular con: '+prome+'</h3>';
         }
         else
         {
-            alert("repobado con: "+prome);
+            var div2 = document.getElementById('califi');
+            div2.innerHTML = '<h3> Reprobado con: '+prome+'</h3>';
         }
     }
 }
@@ -111,22 +136,28 @@ function sentenciaswitch()
     switch(valor)
     {
         case 1:
-            alert("uno");
+            var div = document.getElementById('valor');
+            div.innerHTML = '<h3> uno </h3>';
             break;
         case 2:
-            alert("dos");
+            var div1 = document.getElementById('valor');
+            div1.innerHTML = '<h3> dos </h3>';
             break;
         case 3:
-            alert("tres");
+            var div2 = document.getElementById('valor');
+            div2.innerHTML = '<h3> tres </h3>';
             break;
         case 4:
-            alert("cuatro");
+            var div3 = document.getElementById('valor');
+            div3.innerHTML = '<h3> cuatro </h3>';
             break;
         case 5:
-            alert("cinco");
+            var div4 = document.getElementById('valor');
+            div4.innerHTML = '<h3> cinco </h3>';
             break;
         default:
-            alert("valor fuera de rango");
+            var div5 = document.getElementById('valor');
+            div5.innerHTML = '<h3> valor no valido </h3>';
     }
 }
 
@@ -137,16 +168,23 @@ function sentenciaswitch2()
     switch(col)
     {
         case 'rojo':
+            var div = document.getElementById('color');
+            div.innerHTML = '<h3> color rojo </h3>';
             document.bgColor = 'red';
             break;
         case 'verde':
+            var div1 = document.getElementById('color');
+            div1.innerHTML = '<h3> color verde </h3>';
             document.bgColor = 'green';
             break;
         case 'azul':
+            var div2 = document.getElementById('color');
+            div2.innerHTML = '<h3> color azul </h3>';
             document.bgColor = 'blue';
             break;
         default:
-            alert("color no valido");
+            var div3 = document.getElementById('color');
+            div3.innerHTML = '<h3> color no valido </h3>';
     }
 }
 
@@ -155,7 +193,8 @@ function sentenciawhile()
     var x = 1;
     while(x <= 10)
     {
-        alert(x);
+        var div = document.getElementById('contador');
+        div.innerHTML += '<h3> '+x+' </h3>';
         x++;
     }
 }
@@ -173,8 +212,8 @@ function sentenciawhile2()
         suma += valor;
         x++;
     }
-
-    alert("La suma de los valoes es: "+suma);
+    var div = document.getElementById('sum5');
+    div.innerHTML = '<h3> La suma de los valores es: '+suma+' </h3>';
 }
 
 function sentenciadowhile()
@@ -184,21 +223,27 @@ function sentenciadowhile()
     {
         valor = prompt("ingresa un valor entre 0 y 999: ", '');
         valor = parseInt(valor);
-        alert (" valor ingresado: "+valor);
-        if (valor < 10)
+
+        var div = document.getElementById('ing');
+        div.innerHTML = '<h3> valor ingresado: '+valor+' </h3>';
+
+        if (valor <= 10)
         {
-            alert("tiene 1 digito");
+            var div1 = document.getElementById('dig');
+            div1.innerHTML = '<h3> tiene 1 digito </h3>';
         }else   
         {
-            if (valor < 100)
+            if (valor <= 100)
             {
-                alert("tiene 2 digitos");
+                var div2 = document.getElementById('dig');
+                div2.innerHTML = '<h3> tiene 2 digitos </h3>';
             }else
             {
-                alert("tiene 3 digitos");
+                var div3 = document.getElementById('dig');
+                div3.innerHTML = '<h3> tiene 3 digitos </h3>';
             }
         }
-    }while(valor != 0);
+    }while(valor <= 0);
 }
 
 function sentenciafor()
@@ -206,22 +251,28 @@ function sentenciafor()
     var i;
     for(i = 1; i <= 10; i++)
     {
-        alert(i);
+        var div = document.getElementById('conta');
+        div.innerHTML += '<h3> '+i+' </h3>';
     }
 }
 
 function implementacion()
 {
-    alert("cuiado "+" ingresa tu documento correctamente");
-    alert("cuiado "+" ingresa tu documento correctamente");
-    alert("cuiado "+" ingresa tu documento correctamente");
+    var div = document.getElementById('implementacion');
+    div.innerHTML = '<h3> cuiado '+' ingresa tu documento correctamente</h3>';
+    var div1 = document.getElementById('implementacion');
+    div1.innerHTML += '<h3> cuiado '+' ingresa tu documento correctamente</h3>';
+    var div2 = document.getElementById('implementacion');
+    div2.innerHTML += '<h3> cuiado '+' ingresa tu documento correctamente</h3>';
+
 }
 
 function implementacion2()
 {
     function mensaje()
     {
-        alert("cuiado "+" ingresa tu documento correctamente");
+        var div = document.getElementById('implementacion2');
+        div.innerHTML += '<h3> cuiado '+' ingresa tu documento correctamente</h3>';
     }
     mensaje();
     mensaje();
@@ -239,7 +290,8 @@ function implementacion3()
         var i;
         for(i = valor1; i <= valor2; i++)
         {
-            alert(i);
+            var div = document.getElementById('rango');
+            div.innerHTML += '<h3> '+i+' </h3>';
         }
     }
     mostrarrango(valor1, valor2);
@@ -252,32 +304,44 @@ function retorno()
     {
         if (x == 1)
         {
-            return "uno";
+            var div = document.getElementById('retorno');
+            div.innerHTML = '<h3> uno </h3>';
+            return ;
         }else
         {
             if (x == 2)
             {
-                return "dos";
+                var div1 = document.getElementById('retorno');
+                div1.innerHTML = '<h3> dos </h3>';
+                return ;
             }else
             {
                 if (x == 3)
                 {
-                    return "tres";
+                    var div2 = document.getElementById('retorno');
+                    div2.innerHTML = '<h3> tres </h3>';
+                    return ;
                 }else
                 {
                     if (x == 4)
                     {
-                        return "cuatro";
+                        var div3 = document.getElementById('retorno');
+                        div3.innerHTML = '<h3> cuatro </h3>';
+                        return ;
                     }
                     else
                     {
                         if (x == 5)
                         {
-                            return "cinco";
+                            var div4 = document.getElementById('retorno');
+                            div4.innerHTML = '<h3> cinco </h3>';
+                            return ;
                         }
                         else
                         {
-                            return "numero no valido";
+                            var div5 = document.getElementById('retorno');
+                            div5.innerHTML = '<h3> numero no valido </h3>';
+                            return ;
                         }
                     }
                 }
@@ -286,7 +350,7 @@ function retorno()
     }
     var valor = prompt("ingresa un valor entre 1 y 5: ", '');
     valor = parseInt(valor);
-    alert(convertirCastellano(valor));
+    convertirCastellano(valor);
 }
 
 function retorno2()
@@ -296,20 +360,32 @@ function retorno2()
         switch(x)
         {
             case 1:
-                return "uno";
+                var div = document.getElementById('retorno2');
+                div.innerHTML = '<h3> uno </h3>';
+                break;
             case 2:
-                return "dos";
+                var div1 = document.getElementById('retorno2');
+                div1.innerHTML = '<h3> dos </h3>';
+                break;
             case 3:
-                return "tres";
+                var div2 = document.getElementById('retorno2');
+                div2.innerHTML = '<h3> tres </h3>';
+                break;
             case 4:
-                return "cuatro";
+                var div3 = document.getElementById('retorno2');
+                div3.innerHTML = '<h3> cuatro </h3>';
+                break;
             case 5:
-                return "cinco";
+                var div4 = document.getElementById('retorno2');
+                div4.innerHTML = '<h3> cinco </h3>';
+                break;
             default:
-                return "numero no valido";
+                var div5 = document.getElementById('retorno2');
+                div5.innerHTML = '<h3> numero no valido </h3>';
+                return ;
         }
     }
     var valor = prompt("ingresa un valor entre 1 y 5: ", '');
     valor = parseInt(valor);
-    alert(convertirCastellano(valor));
+    convertirCastellano(valor);
 }
